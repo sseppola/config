8c9f51a7f2a428dd9d78f38a3609e766290d9f2a
Sindre Seppola <sblix.dev@gmail.com>
2020-09-29 20:15:37 +0200

    vscode setup

--- install-apps.sh
+++ install-apps.sh
@@ -13,14 +13,12 @@ fi
 echo "→ Installing applications..."
 # Set up Cask, for Homebrew
 brew tap homebrew/cask
-# Set up Caskroom-versions, which allows apps' alternative versions, like Canary
-brew tap homebrew/versions
 # Install all apps listed in the caskfile
 brew cask install $(cat brew/caskfile|grep -v "#")
 
 echo "→ Installing fonts..."
 # Set up Caskroom-fonts
-brew tap homebrew/fonts
+brew tap homebrew/cask-fonts
 # Install all fonts listed in the fontfile
 brew cask install $(cat brew/fontfile|grep -v "#")
 
--- vscode/setup-vscode.sh
+++ vscode/setup-vscode.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-code --install-extension akamud.vscode-theme-onedark
-code --install-extension alefragnani.bookmarks
-code --install-extension arcticicestudio.nord-visual-studio-code
-code --install-extension PeterJausovec.vscode-docker
-# code --install-extension RobinMalfait.prettier-eslint-vscode
-code --install-extension bibhasdn.unique-lines
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension eg2.tslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension mikestead.dotenv
-code --install-extension mrmlnc.vscode-json5
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension DavidAnson.vscode-markdownlint
+# Install extensions from extensionsFile
+grep -v "#" ./vscode/extensionsFile | while read -r line ; do
+    code --install-extension $line
+done
 
+# Copy VSCode config from this repo into the active vscode config
 rsync -a ./vscode/codeConf/ $HOME/Library/Application\ Support/Code/User/
